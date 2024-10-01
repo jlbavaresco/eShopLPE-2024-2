3ca0bf3fb04cdfc251c7ef2e61fbc031df5ff362
Jorge Luis Boeira Bavaresco <jorge@jorge.ifsul>
2024-10-01 10:14:25 -0300

    tela inicial de produtos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import { getProdutosDB } from "@/componentes/bd/usecases/produtoUseCases";
+import Link from "next/link";
 
 export const revalidate = 60; // tempo em segundos para revalidar
 
@@ -6,16 +7,28 @@ export default async function Home() {
 
   const produtos = await getProdutosDB();
   return (
-    <div style={{padding : '20px'}}>
-      <h1>Produtos disponíveis</h1>
-      { produtos.length > 0 && (
-        produtos.map((produto) => (
-          <div key={produto.codigo}>
-            <h1>{produto.nome}</h1>
-            <h2>{produto.valor}</h2>
-          </div>
-        ))
-      )}
+    <div style={{ padding: '20px' }}>
+      <div className="row">
+        {produtos.length > 0 && (
+          produtos.map((produto) => (
+            <div key={produto.codigo} className="col-sm-3" >
+              <div className="card mb-3 text-center">
+                <div className="card-header">
+                  {produto.nome}
+                </div>
+                <div className="card-body ">
+                  <h5 className="card-title">{produto.nome}</h5>
+                  <p className="card-text"><small className="text-muted">Preço: {produto.valor}</small></p>
+                  <p className="card-text"><small className="text-muted">Categoria: {produto.categoria_nome}</small></p>
+                </div>
+                <div class="card-footer text-muted">
+                  <Link type="button" className="btn btn-secondary" href={`/${produto.codigo}/detalhe`}>Detalhes do produto</Link>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
