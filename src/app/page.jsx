f9f78ae47be247cd7d1bf624c05604788bec6af0
Jorge Luis Boeira Bavaresco <jorge@jorge.ifsul>
2024-10-01 09:17:37 -0300

    produtos no home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,19 @@
+import { getProdutosDB } from "@/componentes/bd/usecases/produtoUseCases";
 
-export default function Home() {
+export default async function Home() {
+
+  const produtos = await getProdutosDB();
   return (
-    <div>
-      <h1>Home</h1>
-      
+    <div style={{padding : '20px'}}>
+      <h1>Produtos disponíveis</h1>
+      { produtos.length > 0 && (
+        produtos.map((produto) => (
+          <div key={produto.codigo}>
+            <h1>{produto.nome}</h1>
+            <h2>{produto.valor}</h2>
+          </div>
+        ))
+      )}
     </div>
   );
 }
